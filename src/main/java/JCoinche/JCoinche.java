de3a5c359805e9e51aec7430ee6c9072462eb25a
Pr0ny <mimoone.ml@gmail.com>
2016-11-26 20:49:32 +0100

    choix des rooms fonctionnel

--- src/main/java/JCoinche/JCoinche.java
+++ src/main/java/JCoinche/JCoinche.java
@@ -1,13 +1,24 @@
 package JCoinche;
 
+import Server.Server;
 import io.netty.bootstrap.Bootstrap;
+
+import java.security.spec.ECField;
 
 /**
  * Created by samue on 25/11/2016.
  */
 public class JCoinche {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        Room room = new Room(500);
+        //System.out.println("Hello World");
+        //Room room = new Room(500);
+        try
+        {
+            Server serv = new Server(4242);
+            serv.run();
+        } catch (Exception e) {
+            System.out.println("Server : " + e.toString());
+        }
+
     }
 }
